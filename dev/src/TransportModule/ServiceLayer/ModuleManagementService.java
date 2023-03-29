c1349444149b079f66c4e8aa413d9da62fa13d78
Yuval Roth <Yuval-Roth@users.noreply.github.com>
2023-03-29 11:25:29 +0300

    Added management service class

---
+++ dev/src/TransportModule/ServiceLayer/ModuleManagementService.java
@@ -0,0 +1,55 @@
+package TransportModule.ServiceLayer;
+
+public class ModuleManagementService {
+
+    public ModuleManagementService(){}
+
+    public String addDriver(String json){
+        return "";
+    }
+
+    public String removeDriver(String json){
+        return "";
+    }
+
+    public String updateDriver(String json){
+        return "";
+    }
+
+    public String getDriver(String json){
+        return "";
+    }
+
+    public String addTruck(String json){
+        return "";
+    }
+
+    public String removeTruck(String json){
+        return "";
+    }
+
+    public String updateTruck(String json){
+        return "";
+    }
+
+    public String getTruck(String json){
+        return "";
+    }
+
+    public String addSite(String json){
+        return "";
+    }
+
+    public String removeSite(String json){
+        return "";
+    }
+
+    public String updateSite(String json){
+        return "";
+    }
+
+    public String getSite(String json){
+        return "";
+    }
+    
+}

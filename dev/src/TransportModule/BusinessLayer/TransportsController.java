4bc81bf384770edbd021b7803b1a57019cd48e6e
Yuval Roth <Yuval-Roth@users.noreply.github.com>
2023-03-29 11:45:14 +0300

    updated TransportsController

--- dev/src/TransportModule/BusinessLayer/TransportsController.java
+++ dev/src/TransportModule/BusinessLayer/TransportsController.java
@@ -1,45 +1,45 @@
 package TransportModule.BusinessLayer;
+
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.TreeSet;
+import java.util.TreeMap;
+
 public class TransportsController {
-    TreeSet <Transport> transports;
+
+    private TreeMap<Integer,Transport> transports;
+    private int idCounter;
+
     public TransportsController(){
-        transports = new TreeSet<>();
+        transports = new TreeMap<>();
+        idCounter = 0; // this will have to be restored from the DB in the future
     }
+
     public Transport createTransport(Site source, LinkedList<Site> destinations, HashMap<Site,ItemList> itemList, int truckID, int DriverID, LocalDateTime date/* int Weight*/){
-        int id = 0;
-        Transport transport = new Transport(id, source, destinations, itemList, truckID, DriverID, date/*Weight*/);
-        transports.add(transport);
-        id++;
+        Transport transport = new Transport(idCounter++, source, destinations, itemList, truckID, DriverID, date/*, Weight*/);
+        transports.put(transport.getId(), transport);
         return transport;
     }
-    public Transport getTransport(int id){
-        for (Transport transport : transports) {
-            if (transport.getId() == id) {
-                return transport;
-            }
-        }
-        return null;
+
+    public Transport getTransport(int id) throws IOException {
+        if (transports.containsKey(id) == false)
+            throw new IOException("Transport not found");
+
+        return transports.get(id);
     }
-    public Transport removeTransport(int id) throws Exception {
-        Transport transport = getTransport(id);
-        if (transport != null) {
-            Transport removedTransport = transport;
-            transports.remove(transport);
-            return removedTransport;
-        }
-        throw new Exception("Transport not found");
+
+    public Transport removeTransport(int id) throws IOException {
+        if (transports.containsKey(id) == false)
+            throw new IOException("Transport not found");
+
+        return transports.remove(id);
     }
-    public boolean updateTransport(int id, Transport newTransport){
-        Transport transport = getTransport(id);
-        if (transport != null) {
-            transports.remove(transport);
-            transports.add(newTransport);
-            return true;
-        }
-        return false;
+    public void updateTransport(int id, Transport newTransport) throws IOException{
+        if(transports.containsKey(id) == false)
+            throw new IOException("Transport not found");
+
+        transports.put(id, newTransport);
     }
 
 }

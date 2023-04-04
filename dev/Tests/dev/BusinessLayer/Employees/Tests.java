7822c9b65327beadee56ec6a519b67fb782a582f
Max <maxbr@post.bgu.ac.il>
2023-04-04 20:31:05 +0300

    tests

--- dev/Tests/dev/BusinessLayer/Employees/Tests.java
+++ dev/Tests/dev/BusinessLayer/Employees/Tests.java
@@ -1,6 +1,7 @@
 package dev.BusinessLayer.Employees;
 
 import dev.ServiceLayer.Objects.Response;
+import dev.ServiceLayer.Objects.SEmployee;
 import dev.ServiceLayer.Services.EmployeesService;
 import dev.ServiceLayer.Services.UserService;
 import org.junit.Assert;
@@ -28,19 +29,19 @@ public class Tests {
         empService = EmployeesService.getInstance();
         userService.loadData(); // Loads the HR Manager user: "admin123" "123", clears the data in each test
         empService.loadData();
-        if(userService.getUser(username2).getReturnValue()==null)
+        admin = userService.getUser(adminUsername).getReturnValue();
+        if(userService.getUser(username2).errorOccurred())
             userService.createUser(admin.getUsername(), username2, password2);
         if(empService.getEmployee(username2).errorOccurred())
             empService.recruitEmployee(admin.getUsername(),"Moshe Biton", "1", username2,"Hapoalim 12 230", 50, LocalDate.of(2023,2,2),"Employment Conditions Test", "More details about Moshe");
-        admin = userService.getUser(adminUsername).getReturnValue();
         user = userService.getUser(username2).getReturnValue();
     }
 
     @Test
     public void recruit_newEmployee() {
         try {
-            Response<Boolean> ans = empService.recruitEmployee(admin.getUsername(),"Max T","1", "555","Hapoalim 12 231", 50, LocalDate.of(2023,2,2),"Employment Conditions Test", "about me");
-            assertTrue(ans.getErrorMessage() == null);
+            Response<Boolean> ans = empService.recruitEmployee(admin.getUsername(),"Max T","2", "555","Hapoalim 12 231", 50, LocalDate.of(2023,2,2),"Employment Conditions Test", "about me");
+            assertTrue(ans.getErrorMessage(),ans.getErrorMessage() == null);
             ans = empService.recruitEmployee(admin.getUsername(),"ab T","2", "555","Hapoalim 12 211", 50, LocalDate.of(2023,2,2),"Employment Conditions Test", "about me");
             assertFalse(ans.getErrorMessage() == null); // existing employee with same ID
             ans = empService.recruitEmployee(username2,"ab T","2", "575","Hapoalim 12 211", 50, LocalDate.of(2023,2,2),"Employment Conditions Test", "about me");
@@ -62,6 +63,28 @@ public class Tests {
         }
     }
 
-    
+    @Test
+    public void checkUserToEmployeeLinkage() {
+        try {
+            String employeeName = "Alex Turner";
+            String employeeId = "989";
+            Response<Boolean> ans = userService.createUser(adminUsername, employeeId, password);
+            assertFalse(ans.errorOccurred());
+            ans = empService.recruitEmployee(admin.getUsername(),employeeName,"2", employeeId,"Hapoalim 12 221", 50, LocalDate.of(2023,2,2),"Employment Conditions Test", "about me");
+            assertFalse(ans.errorOccurred());
+            try{
+            User us = userService.getUser(employeeId).getReturnValue();
+            assertTrue( us != null);
+            Response<SEmployee> as = empService.getEmployee(employeeId);
+            assertTrue(as.getErrorMessage(),as.getReturnValue()!=null);
+            assertTrue(as.getReturnValue().getFullName().equals(employeeName));
+            } catch(Exception e){
+                Assert.fail(e.getMessage());
+            }
+
+        } catch (Exception ignore) {
+            Assert.fail("failed.");
+        }
+    }
    
 }

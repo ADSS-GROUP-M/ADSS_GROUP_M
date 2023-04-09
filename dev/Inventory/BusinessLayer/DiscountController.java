fd8b524d6d782dc6356dbe045d7e5507934078f8
orsaada <saadao@post.bgu.ac.il>
2023-04-09 17:39:30 +0300

    Discount Class

--- dev/Inventory/BusinessLayer/DiscountController.java
+++ dev/Inventory/BusinessLayer/DiscountController.java
@@ -2,31 +2,50 @@ package dev.Inventory.BusinessLayer;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DiscountController {
-    public List<ProductDiscountSupplier> supplierDiscount;
-    public List<ProductStoreDiscount> storeDiscounts;
+    public Map<String, List<ProductDiscountSupplier>> supplierDiscount;
+    public Map<String, List<ProductStoreDiscount>> storeDiscounts;
 
     //should be singleton?
     public DiscountController() {
-        this.supplierDiscount = new ArrayList<ProductDiscountSupplier>();
-        this.storeDiscounts = new ArrayList<ProductStoreDiscount>();
+        this.storeDiscounts = new HashMap<String, List<ProductStoreDiscount>>();
+        this.supplierDiscount = new HashMap<String, List<ProductDiscountSupplier>>();
     }
 
-    public void createSupplierDiscount(int productID, double discount, int supplierID, LocalDateTime startDate, LocalDateTime endDate){
+    public void createSupplierDiscount(int productID, String branch,double discount, int supplierID, LocalDateTime startDate, LocalDateTime endDate){
         //TODO: need to implement
         throw new RuntimeException();
     }
-    public void createStoreDiscount(int productID, double discount, LocalDateTime startDate, LocalDateTime endDate){
+    public void createStoreDiscount(int productID, String branch,double discount, LocalDateTime startDate, LocalDateTime endDate){
         //TODO: need to implement
         throw new RuntimeException();
     }
-    public double getTodayBiggestStoreDiscountI(int productId){
+    public double getTodayBiggestStoreDiscountI(int productID, String branch){
         //TODO: need to implement
         throw new RuntimeException();
     }
-    public double getTodaySupplierDiscountI(int productId){
+    public double getTodaySupplierDiscountI(int productID, String branch){
+        //TODO: need to implement
+        throw new RuntimeException();
+    }
+    public List<ProductStoreDiscount> getStoreDiscountPerDate(int productID, String branch, LocalDateTime startDate, LocalDateTime endDate){
+        //TODO: need to implement
+        throw new RuntimeException();
+    }
+    public double getStoreMinDiscount(int productID, String branch){
+        //TODO: need to implement
+        throw new RuntimeException();
+    }
+
+    public List<ProductStoreDiscount> getSupplierDiscountPerDate(int productID, String branch, int supplierID, LocalDateTime startDate, LocalDateTime endDate){
+        //TODO: need to implement
+        throw new RuntimeException();
+    }
+    public double getSupplierMinDiscount(int productID, String branch, int supplierID){
         //TODO: need to implement
         throw new RuntimeException();
     }

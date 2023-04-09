f37403a21073ee6cff543e96665ad24284a7427d
orsaada <saadao@post.bgu.ac.il>
2023-04-09 16:36:38 +0300

    inventory class and constractors

--- dev/Inventory/BusinessLayer/DiscountController.java
+++ dev/Inventory/BusinessLayer/DiscountController.java
@@ -1,4 +1,34 @@
 package dev.Inventory.BusinessLayer;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DiscountController {
+    public List<ProductDiscountSupplier> supplierDiscount;
+    public List<ProductStoreDiscount> storeDiscounts;
+
+    //should be singleton?
+    public DiscountController() {
+        this.supplierDiscount = new ArrayList<ProductDiscountSupplier>();
+        this.storeDiscounts = new ArrayList<ProductStoreDiscount>();
+    }
+
+    public void createSupplierDiscount(int productID, double discount, int supplierID, LocalDateTime startDate, LocalDateTime endDate){
+        //TODO: need to implement
+        throw new RuntimeException();
+    }
+    public void createStoreDiscount(int productID, double discount, LocalDateTime startDate, LocalDateTime endDate){
+        //TODO: need to implement
+        throw new RuntimeException();
+    }
+    public double getTodayBiggestStoreDiscountI(int productId){
+        //TODO: need to implement
+        throw new RuntimeException();
+    }
+    public double getTodaySupplierDiscountI(int productId){
+        //TODO: need to implement
+        throw new RuntimeException();
+    }
+
 }

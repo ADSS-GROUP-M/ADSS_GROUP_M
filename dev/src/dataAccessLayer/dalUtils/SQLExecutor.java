1ba09626e77954e3f6dc0d45c9891042d7b79dfd
Yuval <rothyuv@post.bgu.ac.il>
2023-04-27 01:14:47 +0300

    removed redundant code

--- dev/src/dataAccessLayer/dalUtils/SQLExecutor.java
+++ dev/src/dataAccessLayer/dalUtils/SQLExecutor.java
@@ -8,13 +8,7 @@ public class SQLExecutor {
 
     private static final String DEFAULT_DB_NAME = "SuperLiDB.db";
     private static final String URL_PREFIX = "jdbc:sqlite:";
-    private static final String USER = "";
-    private static final String PASSWORD = "";
     private final String URL;
-    private static final SQLiteConfig config = new SQLiteConfig(){{
-        enforceForeignKeys(true);
-
-    }};
 
     /**
      * used for production - connects to the {@link #DEFAULT_DB_NAME} database
@@ -33,7 +27,7 @@ public class SQLExecutor {
 
     public OfflineResultSet executeRead(String query) throws SQLException {
 
-        try (Connection connection = DriverManager.getConnection(URL/*, config.toProperties()*/)) {
+        try (Connection connection = DriverManager.getConnection(URL)) {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
             return new OfflineResultSet(resultSet);
@@ -41,7 +35,7 @@ public class SQLExecutor {
     }
 
     public int executeWrite(String query) throws SQLException {
-        try (Connection connection = DriverManager.getConnection(URL/*, config.toProperties()*/)) {
+        try (Connection connection = DriverManager.getConnection(URL)) {
             Statement statement = connection.createStatement();
             query = "PRAGMA foreign_keys = ON;"+query;
             return statement.executeUpdate(query);

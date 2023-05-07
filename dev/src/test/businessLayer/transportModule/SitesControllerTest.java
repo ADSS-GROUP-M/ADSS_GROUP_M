d8e7a3a0515e61c5785e9c231a9d0e3f1040136e
Yuval <rothyuv@post.bgu.ac.il>
2023-05-07 19:47:09 +0300

    Added mocked tests for sites controller

--- dev/src/test/businessLayer/transportModule/SitesControllerTest.java
+++ dev/src/test/businessLayer/transportModule/SitesControllerTest.java
@@ -6,8 +6,11 @@ import dataAccessLayer.transportModule.SitesDistancesDAO;
 import objects.transportObjects.Site;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import serviceLayer.employeeModule.Services.EmployeesService;
 import utils.Response;
 import utils.transportUtils.TransportException;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -23,8 +26,10 @@ class SitesControllerTest {
     @BeforeEach
     void setUp() {
         dao = mock(SitesDAO.class);
+        EmployeesService employeesService = mock(EmployeesService.class);
         distancesDAO = mock(SitesDistancesDAO.class);
         controller = new SitesController(dao, distancesDAO);
+        controller.injectDependencies(employeesService);
 
         site = new Site("zone1", "address1", "phone1", "contact1", Site.SiteType.BRANCH);
     }
@@ -51,34 +56,90 @@ class SitesControllerTest {
 
     @Test
     void removeSite() {
-
+        try{
+            when(dao.exists(Site.getLookupObject(site.address()))).thenReturn(true);
+        } catch (DalException e) {
+            fail(e);
+        }
+        assertDoesNotThrow(() -> controller.removeSite(site.address()));
     }
 
     @Test
     void removeSiteDoesNotExist(){
-
+        try{
+            when(dao.exists(Site.getLookupObject(site.address()))).thenReturn(false);
+        } catch (DalException e) {
+            fail(e);
+        }
+        assertThrows(TransportException.class, () -> controller.removeSite(site.address()));
     }
 
     @Test
     void updateSite() {
-
+        Site updatedSite = new Site("zone1", site.address(), "phone1Updated", "contact1", Site.SiteType.BRANCH);
+        try{
+            Site lookupObject = Site.getLookupObject(site.address());
+            when(dao.exists(lookupObject)).thenReturn(true);
+            when(dao.select(lookupObject)).thenReturn(site);
+        } catch (DalException e) {
+            fail(e);
+        }
+        assertDoesNotThrow(()->controller.updateSite(updatedSite.address(), updatedSite));
     }
 
     @Test
     void updateSiteDoesNotExist(){
-
+        Site updatedSite = new Site("zone1", site.address(), "phone1Updated", "contact1", Site.SiteType.BRANCH);
+        try{
+            when(dao.exists(Site.getLookupObject(site.address()))).thenReturn(false);
+        } catch (DalException e) {
+            fail(e);
+        }
+        assertThrows(TransportException.class, ()->controller.updateSite(updatedSite.address(), updatedSite));
     }
 
     @Test
     void getSite() {
-
+        try{
+            Site lookupObject = Site.getLookupObject(site.address());
+            when(dao.exists(lookupObject)).thenReturn(true);
+            when(dao.select(lookupObject)).thenReturn(site);
+        } catch (DalException e) {
+            fail(e);
+        }
+        Site result = assertDoesNotThrow(()->controller.getSite(site.address()));
+        assertDeepEquals(site, result);
     }
 
     @Test
     void getSiteDoesNotExist(){
+        try{
+            when(dao.exists(Site.getLookupObject(site.address()))).thenReturn(false);
+        } catch (DalException e) {
+            fail(e);
+        }
+        assertThrows(TransportException.class, ()->controller.getSite(site.address()));
     }
 
     @Test
     void getAllSites() {
+        Site site2 = new Site("zone2", "address2", "phone2", "contact2", Site.SiteType.BRANCH);
+        try{
+            when(dao.selectAll()).thenReturn(List.of(site, site2));
+        } catch (DalException e) {
+            fail(e);
+        }
+        List<Site> result = assertDoesNotThrow(()->controller.getAllSites());
+        assertEquals(2, result.size());
+        assertDeepEquals(site, result.get(0));
+        assertDeepEquals(site2, result.get(1));
+    }
+
+    private void assertDeepEquals(Site site1, Site site2) {
+        assertEquals(site1.transportZone(), site2.transportZone());
+        assertEquals(site1.address(), site2.address());
+        assertEquals(site1.contactName(), site2.contactName());
+        assertEquals(site1.phoneNumber(), site2.phoneNumber());
+        assertEquals(site1.siteType(), site2.siteType());
     }
 }

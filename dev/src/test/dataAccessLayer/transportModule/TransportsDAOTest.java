27f665d31f108dca949da1258bb8fd0829e14c6d
Yuval <rothyuv@post.bgu.ac.il>
2023-05-14 01:51:40 +0300

    fixed transportsDAOTest

--- dev/src/test/dataAccessLayer/transportModule/TransportsDAOTest.java
+++ dev/src/test/dataAccessLayer/transportModule/TransportsDAOTest.java
@@ -15,9 +15,11 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import static dataAccessLayer.DalFactory.TESTING_DB_NAME;
 import static org.junit.jupiter.api.Assertions.*;
@@ -36,22 +38,36 @@ class TransportsDAOTest {
     private ItemList itemList;
     private Driver driver;
     private Truck truck;
-    private Site site;
+    private Site dest1;
+    private Site dest2;
     private Site source;
     private Employee employee;
     private SitesRoutesDAO routesDAO;
-    private TransportsController transportsController;
+    private SiteRoute siteRoute1;
+    private SiteRoute siteRoute2;
+    private Map<String, LocalTime> arrivalTimes;
 
     @BeforeEach
     void setUp() {
-        site = new Site("dest1", "dest1 name", "zone1", "12345","kobi", Site.SiteType.SUPPLIER);
+
+        DalFactory.clearTestDB();
+
+        //sites set up
+        source = new Site("source1", "source1 name", "zone1", "12345","kobi", Site.SiteType.SUPPLIER);
+        dest1 = new Site("dest1", "dest1 name", "zone1", "12345","kobi", Site.SiteType.SUPPLIER);
+        dest2 = new Site("dest2", "dest2 name", "zone1", "12345","kobi", Site.SiteType.SUPPLIER);
         source = new Site("source1", "source1 name", "zone1", "12345","kobi", Site.SiteType.BRANCH);
+
+        //truck set up
         truck = new Truck("1", "model1", 1000, 20000, Truck.CoolingCapacity.FROZEN);
+
+        //driver set up
         employee = new Employee("name1","12345","Poalim",50, LocalDate.of(1999,10,10),"conditions","details");
         employee.addRole(Role.Driver);
         employee.addRole(Role.GeneralWorker);
         driver = new Driver(employee.getId(),employee.getName(), Driver.LicenseType.C3);
 
+        //itemList set up
         HashMap<String, Integer> load = new HashMap<>(){{
             put("item1", 1);
             put("item2", 2);
@@ -64,31 +80,39 @@ class TransportsDAOTest {
         }};
         itemList = new ItemList(1, load, unload);
 
+        //transport set up
+        siteRoute1 = new SiteRoute(
+                source.address(),
+                dest1.address(),
+                10,10
+        );
+        siteRoute2 = new SiteRoute(
+                dest1.address(),
+                dest2.address(),
+                10,10
+        );
         transport = new Transport(1,
                 source.name(),
                 new LinkedList<>(){{
-                    add(site.name());
+                    add(dest1.name());
+                    add(dest2.name());
                 }} ,
                 new HashMap<>(){{
-                    put(site.name(), 1);
+                    put(dest1.name(), itemList.id());
+                    put(dest2.name(), itemList.id());
                 }},
                 driver.id(),
                 truck.id(),
-                LocalDateTime.of(2020, 1, 1, 1, 1),
+                LocalDateTime.of(2023, 2, 2, 12, 0),
                 15000
         );
 
-        SiteRoute siteRoute1 = new SiteRoute(
-                source.address(),
-                site.address(),
-                100,100
-        );
+        //initialize transport arrival times
+        arrivalTimes = new HashMap<>();
+        arrivalTimes.put(dest1.name(), LocalTime.of(12, (int) siteRoute1.duration()));
+        arrivalTimes.put(dest2.name(), LocalTime.of(12, (int)(siteRoute1.duration() + siteRoute2.duration() + TransportsController.AVERAGE_TIME_PER_VISIT)));
+        transport.deliveryRoute().initializeArrivalTimes(arrivalTimes);
 
-        SiteRoute siteRoute2 = new SiteRoute(
-                source.address(),
-                source.address(),
-                100,100
-        );
 
         try {
             DalFactory factory = new DalFactory(TESTING_DB_NAME);
@@ -99,41 +123,19 @@ class TransportsDAOTest {
             itemListsDAO = factory.itemListsDAO();
             transportsDAO = factory.transportsDAO();
             routesDAO = factory.sitesRoutesDAO();
-            TrucksController trucksController = mock(TrucksController.class);
-            ItemListsController itemListsController = mock(ItemListsController.class);
-            DriversController driversController = mock(DriversController.class);
-            SitesRoutesController sitesRoutesController = mock(SitesRoutesController.class);
-            SitesController sitesController =  new SitesController(sitesDAO, sitesRoutesController);
-            transportsController = new TransportsController(
-                    trucksController,
-                    driversController,
-                    sitesController,
-                    itemListsController,
-                    sitesRoutesController,
-                    transportsDAO
-            );
 
-            transportsDAO.clearTable();
-            itemListsDAO.clearTable();
-            driversDAO.clearTable();
-            trucksDAO.clearTable();
-            sitesDAO.clearTable();
-            employeeDAO.clearTable();
-
-            sitesDAO.insert(site);
             sitesDAO.insert(source);
+            sitesDAO.insert(dest1);
+            sitesDAO.insert(dest2);
             routesDAO.insert(siteRoute1);
             routesDAO.insert(siteRoute2);
             trucksDAO.insert(truck);
             employeeDAO.insert(employee);
             driversDAO.insert(driver);
             itemListsDAO.insert(itemList);
-            transportsController.initializeEstimatedArrivalTimes(transport);
-            transportsDAO.insert(transport);
-        } catch (DalException | TransportException e) {
+        } catch (DalException e) {
             fail(e);
         }
-        transportsDAO.clearCache();
     }
 
     @AfterEach
@@ -142,8 +144,10 @@ class TransportsDAOTest {
     }
 
     @Test
-    void select() {
+    void insert_and_select_success() {
         try {
+            transportsDAO.insert(transport);
+            transportsDAO.clearCache();
             Transport selected = transportsDAO.select(transport);
             assertDeepEquals(transport, selected);
         } catch (DalException e) {
@@ -155,29 +159,19 @@ class TransportsDAOTest {
     void selectAll() {
         
         //set up
-        SiteRoute distance = new SiteRoute(
-                site.address(),
-                site.address(),
-                100,100
-        );
-        try {
-            routesDAO.insert(distance);
-        } catch (DalException e) {
-            fail(e);
-        }
-
         LinkedList<Transport> transports = new LinkedList<>();
-        transports.add(transport);
-        List.of(2,3,4,5,6).forEach(
+        List.of(1,2,3,4,5).forEach(
             i -> {
                 try {
                     Transport toAdd = new Transport(i,
-                            site.name(),
+                            source.name(),
                             new LinkedList<>(){{
-                                add(site.name());
+                                add(dest1.name());
+                                add(dest2.name());
                             }} ,
                             new HashMap<>(){{
-                                put(site.name(), 1);
+                                put(dest1.name(), itemList.id());
+                                put(dest2.name(), itemList.id());
                             }},
                             driver.id(),
                             truck.id(),
@@ -185,9 +179,9 @@ class TransportsDAOTest {
                             15000
                     );
                     transports.add(toAdd);
-                    transportsController.initializeEstimatedArrivalTimes(toAdd);
+                    toAdd.deliveryRoute().initializeArrivalTimes(arrivalTimes);
                     transportsDAO.insert(toAdd);
-                } catch (DalException | TransportException e) {
+                } catch (DalException e) {
                     fail(e);
                 }
             }
@@ -208,60 +202,32 @@ class TransportsDAOTest {
         }
     }
 
-    @Test
-    void insert() {
-        try {
-            Transport transport2 = new Transport(2,
-                    source.name(),
-                    new LinkedList<>() {{
-                        add(source.name());
-                        add(site.name());
-                    }},
-                    new HashMap<>() {{
-                        put(source.name(), 1);
-                        put(site.name(), 1);
-                    }},
-                    driver.id(),
-                    truck.id(),
-                    LocalDateTime.of(2020, 1, 1, 1, 1),
-                    15000
-            );
-            try {
-                transportsController.initializeEstimatedArrivalTimes(transport2);
-            } catch (TransportException e) {
-                fail(e);
-            }
-            transportsDAO.insert(transport2);
-            Transport selected = transportsDAO.select(Transport.getLookupObject(transport2.id()));
-            assertDeepEquals(transport2, selected);
-        } catch (DalException e) {
-            fail(e);
-        }
-    }
 
     @Test
     void update() {
         try {
+            //set up
             Transport updatedTransport = new Transport(transport.id(),
                     source.name(),
                     new LinkedList<>() {{
-                        add(site.name());
+                        add(dest1.name());
                     }},
                     new HashMap<>() {{
-                        put(site.name(), 1);
+                        put(dest1.name(), itemList.id());
                     }},
                     driver.id(),
                     truck.id(),
                     LocalDateTime.of(1997, 2, 2, 2, 2),
                     10000
             );
-            try {
-                transportsController.initializeEstimatedArrivalTimes(updatedTransport);
-            } catch (TransportException e) {
-                fail(e);
-            }
-            transportsDAO.update(updatedTransport);
+            arrivalTimes.remove(dest2.name());
+            updatedTransport.deliveryRoute().initializeArrivalTimes(arrivalTimes);
+            transportsDAO.insert(transport);
+            transportsDAO.clearCache();
 
+            //test
+            transportsDAO.update(updatedTransport);
+            transportsDAO.clearCache();
             Transport selected = transportsDAO.select(Transport.getLookupObject(transport.id()));
             assertDeepEquals(updatedTransport, selected);
         } catch (DalException e) {
@@ -272,6 +238,13 @@ class TransportsDAOTest {
     @Test
     void delete() {
         try {
+            //set up
+            transportsDAO.insert(transport);
+            transportsDAO.clearCache();
+            assertDoesNotThrow(() -> transportsDAO.select(Transport.getLookupObject(transport.id())));
+            transportsDAO.clearCache();
+
+            //test
             transportsDAO.delete(transport);
             assertThrows(DalException.class, () -> transportsDAO.select(Transport.getLookupObject(transport.id())));
         } catch (DalException e) {

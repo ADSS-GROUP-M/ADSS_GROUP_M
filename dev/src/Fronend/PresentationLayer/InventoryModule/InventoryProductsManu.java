510bc316873a3f5683c19fc4cfe6b4b148932833
yossiHad <yossihadad101@gmail.com>
2023-05-16 00:09:29 +0300

    creating main menu

--- dev/src/Fronend/PresentationLayer/InventoryModule/InventoryProductsManu.java
+++ dev/src/Fronend/PresentationLayer/InventoryModule/InventoryProductsManu.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class InventoryProductsManu extends MainMenu {
+public class InventoryProductsManu extends InventoryModule {
 
     private String branch;
 
@@ -28,7 +28,7 @@ public class InventoryProductsManu extends MainMenu {
             case 3 -> updateProduct();
             case 4 -> updateItem();
             case 5 -> getProductDetails();
-            case 6 -> new MainMenu().Menu();
+            case 6 -> new InventoryModule().Menu();
             case 7 -> System.exit(0);
             default -> System.out.println("\nInvalid command");
         }

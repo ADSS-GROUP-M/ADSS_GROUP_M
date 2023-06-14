96a540c4eeb3b9e24be4f79a54e8cb77f51abfe3
Hila elbaz <hilaelb@post.bgu.ac.il>
2023-06-14 18:10:15 +0300

    add time and date in the addTransportPanel

---
+++ dev/src/presentationLayer/gui/plUtils/DateAndTimeExample.java
@@ -0,0 +1,34 @@
+package presentationLayer.gui.plUtils;
+import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateAndTimeExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("DateTime Panel Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+
+        // Create a date field
+        JLabel dateLabel = new JLabel("Date: ");
+        panel.add(dateLabel);
+
+        JFormattedTextField dateField = new JFormattedTextField(new SimpleDateFormat("yyyy-MM-dd"));
+        dateField.setColumns(10);
+        panel.add(dateField);
+
+        // Create a time field
+        JLabel timeLabel = new JLabel("Time: ");
+        panel.add(timeLabel);
+
+        JFormattedTextField timeField = new JFormattedTextField(new SimpleDateFormat("HH:mm:ss"));
+        timeField.setColumns(8);
+        panel.add(timeField);
+
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}

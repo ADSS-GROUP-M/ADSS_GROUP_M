e15548dfa3233bbfbd68f1e3da0ba13bdfd6aed0
Yuval Roth <Yuval-Roth@users.noreply.github.com>
2023-06-18 13:09:59 +0300

    added doc

--- dev/src/presentationLayer/gui/plAbstracts/interfaces/UIElementObserver.java
+++ dev/src/presentationLayer/gui/plAbstracts/interfaces/UIElementObserver.java
@@ -1,7 +1,12 @@
 package presentationLayer.gui.plAbstracts.interfaces;
 
-import java.util.List;
-
+/**
+ * Observer interface for UIElements. <br/>
+ * This interface contains empty default methods for all the methods.<br/>
+ * This is done so that the observer can choose which events to catch.
+ *
+ * @see ObservableUIElement
+ */
 public interface UIElementObserver {
     default void add(ObservableUIElement observable){}
     default void remove(ObservableUIElement observable) {}
